343b9a900315d9a070c00fd419a70a4bf8e544a1
araventh <araventh04@gmail.com>
2021-05-31 03:15:32 +0530

    connections

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const client = new Pool({
     host: "localhost",
     port: 5432,
     user: "postgres",
-    password: "11babli22",
+    password: "Dhanush@04",
     database: "dbms_project"
 })
 
@@ -24,6 +24,7 @@ app.get("/",function(req,res){
 });
 
 var result="";
+var bresult="";
 app.post("/",function(req,res){
     f=2;
     var but = req.body.submitform;
@@ -40,19 +41,27 @@ app.post("/",function(req,res){
             let uweight = req.body.user_weight;
             await client.connect();
             await client.query('INSERT INTO USERS(UNAME,EMAIL,UPASSWORD,GENDER,WEIGHT,HEIGHT) VALUES ($1,$2,$3,$4,$5,$6)',[uname,uemail,upass,ugenger,uweight,uheight]);
-            const resu = await client.query('SELECT UNAME FROM USERS WHERE EMAIL=$1 AND UPASSWORD=$2',[uemail,upass]);
-            result=resu.rows[0].uname;
+            var resu = await client.query('SELECT * FROM USERS WHERE EMAIL=$1 AND UPASSWORD=$2',[uemail,upass]);
+            result=resu.rows[0];
+            resu = await client.query('SELECT * FROM bmi WHERE email=$1',[uemail]);
+            
+            bresult = resu.rows[0];
+            console.log(bresult);
             console.log(result);
-            res.render("index",{flag:f,use_name: result});
+            res.render("index",{flag:f,use_name: result.uname});
         })();
     }
     else{
         (async () => {
             await client.connect();
-            const resu = await client.query('SELECT UNAME FROM USERS WHERE EMAIL=$1 AND UPASSWORD=$2',[uemail,upass]);
-            result=resu.rows[0].uname;
+            var resu = await client.query('SELECT * FROM USERS WHERE EMAIL=$1 AND UPASSWORD=$2',[uemail,upass]);
+            result=resu.rows[0];
+            resu = await client.query('SELECT * FROM bmi WHERE email=$1',[uemail]);
+            bresult = resu.rows[0];
+            console.log(bresult);
+            
             console.log(result);
-            res.render("index",{flag:f,use_name: result});
+            res.render("index",{flag:f,use_name: result.uname});
         })();
     }
     
@@ -63,10 +72,19 @@ app.get("/about",function(req,res){
     res.render("about");
 });
 app.get("/dietplan",function(req,res){
-    res.render("dietplan",{flag:f,use_name: result,i:1});
+    res.render("dietplan",{flag:f,use_name: result.uname,i:1});
 });
-app.get("/user",function(req,res){
-    res.render("user",{flag:f,use_name: result});
+app.get("/user",async(req,res)=>{
+    if(result.gender == 'M')
+        var g='Male';
+    else
+        var g='Female';
+    await client.connect();    
+    console.log(result.email);
+    var resu = await client.query('select * from bmi where email=$1',[result.email]);
+    console.log(resu.rows);
+    var ress = await client.query('select uname from users where email in (select remail from connections where semail=$1)',[result.email]);
+    res.render("user",{flag:f,use_name: result.uname,gen:g,b:bresult.bmi,wei:bresult.weight,hei:bresult.height,bmihis:resu.rows,myconnect:ress.rows,p:1});
     // res.render("user");
 });
 app.post("/dietplan",async(req,res)=>{
@@ -102,10 +120,62 @@ app.post("/dietplan",async(req,res)=>{
     
 
     console.log(bf[0].foods);
-    res.render("dietplan",{flag:f,use_name: result,i:2,bfarr:bf,lunarr:lun,dinarr:din});
-})
-app.get("/dietplan/diet", (res,req)=>{
+    res.render("dietplan",{flag:f,use_name: result.uname,i:2,bfarr:bf,lunarr:lun,dinarr:din});
+});
+app.get('/users',async(req,res)=>{
+    const searchField=req.query.x;
+    try{
+        await client.connect();
+        var resu=await client.query("select uname,email from users where email != $1 and uname like $2 || '%' and email not in (select remail from connections where semail=$3 )",[result.email,searchField,result.email]);
+        console.log(resu.rows);
+        return res.status(200).send({message:"Fetch successful",data:resu.rows});
+    }
+    catch(err){
+        console.log(err.message);
+        return res.status(400).send({message:"Fetch unsuccessful"});
+    }
+});
 
-    return 2;
+app.post('/user',async(req,res)=>{
+    var a = req.body.weig;
+    var b = req.body.heig;
+    c=b/a;
+    await client.connect();
+    await client.query('insert into bmi(email,weight,height,bmi,gender) values($1,$2,$3,$4,$5)',[result.email,a,b,c,result.gender]);
+    var resu = await client.query('select * from bmi where email=$1',[result.email]);
+    res.render("user",{flag:f,use_name: result.uname,gen:result.gender,b:c,wei:a,hei:b,bmihis:resu,myconnect:ress.rows,p:1});
 })
+
+app.get('/use',async(req,res)=>{
+    const addfriend = req.query.y;
+    console.log(addfriend);
+    try{
+        await client.connect();
+        var ress = await client.query('select email from users where uname =$1',[addfriend]);
+        var resu=await client.query('insert into connections values($1,$2,$3)',[result.email,ress.rows[0].email,'1']);
+        console.log(ress.rows[0].email);
+        return res.status(200).send({message:"Fetch successful",data:resu.rows});
+    }
+    catch(err){
+        console.log(err.message);
+        return res.status(400).send({message:"Fetch unsuccessful"});
+    }
+})
+
+app.get('/us',async(req,res)=>{
+    const addfriend = req.query.z;
+    console.log(addfriend);
+    try{
+        await client.connect();
+        var ress = await client.query('select * from users where uname =$1',[addfriend]);
+        console.log(ress.rows[0].email);
+        c= ress.rows[0].height/ress.rows[0].weight;
+        return res.status(200).send({message:"Fetch successful",data:ress.rows});
+    }
+    catch(err){
+        console.log(err.message);
+        return res.status(400).send({message:"Fetch unsuccessful"});
+    }
+})
+
 app.listen(3000);
